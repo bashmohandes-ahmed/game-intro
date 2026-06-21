9c4c853bf3b8986ec27c44164750cb0521f8ece5
Ahmed Farag <ahmedusamaofficial1@gmail.com>
2026-06-22 02:54:47 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+question1 = "Enter Your age:"
+question2 = "Enter Your Name:"
+question3 = "Enter Your level:"
+answer1 = input(question1)
+answer2 = input(question2)
+answer3 = input(question3)
+print(answer1)
+print(answer2)
+print(answer3)
+print("Your information has been Confirmed")
